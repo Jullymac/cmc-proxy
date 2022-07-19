c6d82c43b8ca13b0489cbd9d778a621ba30c800b
Juliana Macedo <jmacedo@devexperts.com>
2022-07-19 09:54:08 +0100

    Fix request

--- proxy.js
+++ proxy.js
@@ -1,29 +1,30 @@
-const express = require('express')
-const cors = require('cors')
-const morgan = require('morgan')
-const axios = require('axios')
+const express = require("express");
+const cors = require("cors");
+const morgan = require("morgan");
+const axios = require("axios");
 
-require('dotenv').config()
+require("dotenv").config();
 
-const app = express()
+const app = express();
 
-app.use(morgan('tiny'))
-app.use(cors())
+app.use(morgan("tiny"));
+app.use(cors());
 
-app.get('/*', (req,res) => {
-    let url = `https://pro-api.coinmarketcap.com/v1${req.originalUrl}`
-    
-    axios.get(url,{headers: { 'X-CMC_PRO_API_KEY':process.env.CMC_API_KEY }})
-        .then(response => {
-            res.send(response.data)
-        })
-        .catch(err => {
-            console.log(err.response.data)
-            res.send(err.response.data)
-        })
-})
+app.get("/*", (req, res) => {
+  let url = `https://pro-api.coinmarketcap.com${req.originalUrl}`;
 
-const port = process.env.PORT || 5000
+  axios
+    .get(url, { headers: { "X-CMC_PRO_API_KEY": process.env.CMC_API_KEY } })
+    .then((response) => {
+      res.send(response.data);
+    })
+    .catch((err) => {
+      console.log(err.response.data);
+      res.send(err.response.data);
+    });
+});
+
+const port = process.env.PORT || 3003;
 app.listen(port, () => {
-    console.log('Listening on port ',port)
-})
+  console.log("Listening on port ", port);
+});
